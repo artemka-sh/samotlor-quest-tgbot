39aef57c2b33ee2591dc8c68036dfefa596c4496
artemka-sh <artemmmchik@mail.ru>
2025-06-26 14:19:09 +0300

    changes 1

--- include/config.hpp
+++ include/config.hpp
@@ -1,27 +1,38 @@
 #pragma once
 
-#include "json.hpp"
+#include <boost/json.hpp>
 #include <fstream>
 
-using json = nlohmann::json;
+using json = boost::json::value;
 
 class Config
 {
 public:    
     json data;
     std::string telegram_token;
+    std::string db_addres;
+    std::string db_port;
+    std::string db_user;
+    std::string db_password;
+
     Config()
     {
         readConfigFromFile("res/config.json");
-        telegram_token = data["telegram_token"];
+        telegram_token = data.as_object()["telegram_token"].as_string().c_str();
+        db_addres = data.as_object()["db_addres"].as_string().c_str();
+        db_port = data.as_object()["db_port"].as_string().c_str();
+        db_user = data.as_object()["db_user"].as_string().c_str();
+        db_password = data.as_object()["db_password"].as_string().c_str();
     }
     
 private:
     void readConfigFromFile(std::string path)
     {
-        if(path.empty()) throw std::runtime_error("config path empty");
+        if (path.empty()) throw std::runtime_error("config path empty");
 
         std::ifstream configfile(path);
-        data = json::parse(configfile);
+        std::string content((std::istreambuf_iterator<char>(configfile)),
+                            std::istreambuf_iterator<char>());
+        data = boost::json::parse(content);
     }
 };
